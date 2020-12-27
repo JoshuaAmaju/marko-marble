d43c993f980efb40f4b6f212f954410d99973150
Joshua Amaju <princejosh0007@gmail.com>
2020-12-27 19:22:43 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
+import "marko/express";
 import express from "express";
+import compression from "compression";
+import serveStatic from "serve-static";
+
+import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Enable gzip compression for all HTTP responses
-import compression from "compression";
 app.use(compression());
 
 // Allow all of the generated files to be served up by Express
-import serveStatic from "serve-static";
 app.use("/static", serveStatic("dist/client"));
 
 // Initialize mock service routes
-import initServices from "./services/routes";
-initServices(app);
+// initServices(app);
 
 // Map the "/" route to the home page
-import HomePage from "./pages/home";
 app.get("/", HomePage);
+app.get("/about", AboutPage);
 
 // Start the server
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     throw err;
   }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,5 @@
 import template from "./template.marko";
 
 export default (req, res) => {
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
-  template.render({}, res);
+  template.stream({}).pipe(res);
 };
